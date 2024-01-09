457ab7dd02fb3f35d4c5a9198868c1fda2aecca4
Jun <wnsgur4092@gmail.com>
2024-01-10 04:42:01 +1100

    Initial commit

---
+++ Workhour/WorkhourApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkhourApp.swift
+//  Workhour
+//
+//  Created by JunHyuk Lim on 10/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkhourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
