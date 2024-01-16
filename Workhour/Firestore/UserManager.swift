b947d81b2b618b1b81a0e07fc9ce454498b7b0e2
Jun <wnsgur4092@gmail.com>
2024-01-16 20:29:13 +1100

    Refactoring user data model

--- Workhour/Firestore/UserManager.swift
+++ Workhour/Firestore/UserManager.swift
@@ -13,6 +13,12 @@ struct DBUser: Codable {
     let userId : String
     let email : String?
     let photoUrl : String?
+    
+    init(auth: AuthDataResultModel){
+        self.userId = auth.uid
+        self.email = auth.email
+        self.photoUrl = auth.photoUrl
+    }
 }
 
 final class UserManager{
@@ -32,34 +38,46 @@ final class UserManager{
         return encoder
     }()
     
+    private let decoder : Firestore.Decoder = {
+        let decoder = Firestore.Decoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    
     func createNewUser(user: DBUser) async throws{
         try userDocument(userId: user.userId).setData(from: user,merge: false, encoder: encoder)
     }
     
-    func createNewUser(auth: AuthDataResultModel) async throws{
-        var userData : [String:Any] = [
-            "user_id" : auth.uid
-        ]
-        if let email = auth.email{
-            userData["email"] = email
-        }
-        if let photoUrl = auth.photoUrl{
-            userData["photo_url"] = photoUrl
-        }
-        
-        try await userDocument(userId: auth.uid).setData(userData, merge: false)
-    }
+//    func createNewUser(auth: AuthDataResultModel) async throws{
+//        var userData : [String:Any] = [
+//            "user_id" : auth.uid
+//        ]
+//        if let email = auth.email{
+//            userData["email"] = email
+//        }
+//        if let photoUrl = auth.photoUrl{
+//            userData["photo_url"] = photoUrl
+//        }
+//        
+//        try await userDocument(userId: auth.uid).setData(userData, merge: false)
+//    }
     
     func getUser(userId: String) async throws -> DBUser{
-        let snapshot = try await userDocument(userId: userId).getDocument()
-        
-        guard let data = snapshot.data(), let userId = data["user_id"] as? String  else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let email = data["email"] as? String
-        let photoUrl = data["photo_url"] as? String
-        
-        return DBUser(userId: userId, email: email, photoUrl: photoUrl)
+        try await userDocument(userId: userId).getDocument(as: DBUser.self, decoder: decoder)
     }
+    
+    
+//    func getUser(userId: String) async throws -> DBUser{
+//        let snapshot = try await userDocument(userId: userId).getDocument()
+//        
+//        guard let data = snapshot.data(), let userId = data["user_id"] as? String  else {
+//            throw URLError(.badServerResponse)
+//        }
+//        
+//        let email = data["email"] as? String
+//        let photoUrl = data["photo_url"] as? String
+//        
+//        return DBUser(userId: userId, email: email, photoUrl: photoUrl)
+//    }
 }
